d40fdb5061b37255e611888e06c60071de4694b3
Francisco Riordan <cisco.riordan@gmail.com>
2026-04-09 16:11:09 -0700

    Bump swift-sdk to 0.12.0 to fix Swift 6 concurrency build error

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.1.0"),
-        .package(url: "https://github.com/modelcontextprotocol/swift-sdk.git", from: "0.11.0"),
+        .package(url: "https://github.com/modelcontextprotocol/swift-sdk.git", from: "0.12.0"),
     ],
     targets: [
         // Build plugin: generates Version.swift from the VERSION file
